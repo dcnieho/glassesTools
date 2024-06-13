df2f0e0e9d2ef75b071abf4361c85e6803cce6d3
Diederick Niehorster <dcnieho@gmail.com>
2024-06-13 02:01:17 +0200

    gaze_headref: added optional extra timestamp and frame_idx columns

--- src/glassesTools/gaze_headref.py
+++ src/glassesTools/gaze_headref.py
@@ -7,28 +7,45 @@ from . import data_files, drawing, ocv
 
 class Gaze:
     # description of tsv file used for storage
-    _columns_compressed = {'timestamp': 1, 'frame_idx':1,
+    _columns_compressed = {'timestamp': 1, 'timestamp_VOR': 1, 'frame_idx':1, 'frame_idx_VOR':1,
                            'gaze_pos_vid': 2, 'gaze_pos_3d': 3, 'gaze_dir_l': 3, 'gaze_ori_l': 3, 'gaze_dir_r': 3, 'gaze_ori_r': 3}
-    _non_float          = {'frame_idx': int}
+    _non_float          = {'frame_idx': int, 'frame_idx_VOR': int, 'frame_idx_ref': int}
 
     def __init__(self,
                  timestamp      : float,
                  frame_idx      : int,
+
                  gaze_pos_vid   : np.ndarray,
+
+                 timestamp_ori  : float      = None,
+                 frame_idx_ori  : int        = None,
+                 timestamp_VOR  : float      = None,
+                 frame_idx_VOR  : int        = None,
+                 timestamp_ref  : float      = None,
+                 frame_idx_ref  : int        = None,
+
                  gaze_pos_3d    : np.ndarray = None,
                  gaze_dir_l     : np.ndarray = None,
                  gaze_ori_l     : np.ndarray = None,
                  gaze_dir_r     : np.ndarray = None,
                  gaze_ori_r     : np.ndarray = None):
-        self.timestamp   : float        = timestamp
-        self.frame_idx   : int          = frame_idx
+        self.timestamp    : float       = timestamp
+        self.frame_idx    : int         = frame_idx
 
-        self.gaze_pos_vid: np.ndarray   = gaze_pos_vid      # gaze point on the scene video
-        self.gaze_pos_3d : np.ndarray   = gaze_pos_3d       # gaze point in the world (often binocular gaze point)
-        self.gaze_dir_l  : np.ndarray   = gaze_dir_l
-        self.gaze_ori_l  : np.ndarray   = gaze_ori_l
-        self.gaze_dir_r  : np.ndarray   = gaze_dir_r
-        self.gaze_ori_r  : np.ndarray   = gaze_ori_r
+        # optional timestamps and frame_idxs
+        self.timestamp_ori: float       = timestamp_ori     # timestamp field can be set to any of these three. Keep here a copy of the original timestamp
+        self.frame_idx_ori: int         = frame_idx_ori
+        self.timestamp_VOR: float       = timestamp_VOR
+        self.frame_idx_VOR: int         = frame_idx_VOR
+        self.timestamp_ref: float       = timestamp_ref
+        self.frame_idx_ref: int         = frame_idx_ref     # frameidx _in reference video_ not in this eye tracker's video (unless this is the reference)
+
+        self.gaze_pos_vid : np.ndarray  = gaze_pos_vid      # gaze point on the scene video
+        self.gaze_pos_3d  : np.ndarray  = gaze_pos_3d       # gaze point in the world (often binocular gaze point)
+        self.gaze_dir_l   : np.ndarray  = gaze_dir_l
+        self.gaze_ori_l   : np.ndarray  = gaze_ori_l
+        self.gaze_dir_r   : np.ndarray  = gaze_dir_r
+        self.gaze_ori_r   : np.ndarray  = gaze_ori_r
 
     def draw(self, img:np.ndarray, cameraParams:ocv.CameraParams=None, subPixelFac=1):
         drawing.openCVCircle(img, self.gaze_pos_vid, 8, (0,255,0), 2, subPixelFac)
@@ -41,7 +58,7 @@ class Gaze:
             drawing.openCVCircle(img, a, 5, (0,255,255), -1, subPixelFac)
 
 
-def read_dict_from_file(fileName:str|pathlib.Path, episodes:list[list[int]]=None) -> tuple[dict[int,list[Gaze]], int]:
+def read_dict_from_file(fileName:str|pathlib.Path, episodes:list[list[int]]=None, ts_column_suffixes: list[str] = None) -> tuple[dict[int,list[Gaze]], int]:
     return data_files.read_file(fileName,
-                                Gaze, False, False, True,
-                                episodes=episodes)
+                                Gaze, False, False, True, True,
+                                episodes=episodes, ts_fridx_field_suffixes=ts_column_suffixes)
