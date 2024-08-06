154b6c1c318cbd832205266fae7ae208dc41d1f2
Diederick Niehorster <dcnieho@gmail.com>
2024-08-06 09:42:50 +0200

    utils.unpack_none_union: typing and return if input as a union with None

--- src/glassesTools/utils.py
+++ src/glassesTools/utils.py
@@ -94,9 +94,9 @@ def fast_scandir(dirname):
         subfolders.extend(fast_scandir(dirname))
     return subfolders
 
-def unpack_none_union(annotation):
+def unpack_none_union(annotation: typing.Type) -> tuple[typing.Type, bool]:
     # below handles both types.Optional and direct unions with None
     if typing.get_origin(annotation) in [typing.Union, types.UnionType] and (args:=typing.get_args(annotation))[-1]==types.NoneType:
-        return typing.Union[args[:-1]]
+        return typing.Union[args[:-1]], True
     else:
-        return annotation
+        return annotation, False
