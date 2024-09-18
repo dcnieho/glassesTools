63cb580982a6aaeefade022e74f161c47f020659
Diederick Niehorster <dcnieho@gmail.com>
2024-09-18 16:09:49 +0200

    gui submodule: import all its submodules

--- src/glassesTools/gui/__init__.py
+++ src/glassesTools/gui/__init__.py
@@ -3,3 +3,5 @@ try:
     import imgui_bundle
 except ImportError:
     raise ImportError('imgui_bundle (or one of its dependencies) is not installed, GUI functionality is not available. You must install glassesTools with the [GUI] extra if you wish to use the GUI.') from None
+
+from . import file_picker, msg_box, recording_table, signal_sync, timeline, utils, video_player, worldgaze
