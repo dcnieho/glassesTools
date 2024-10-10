47cdd85c6cc45db8e9e0f3a82433978c49c57da2
Diederick Niehorster <dcnieho@gmail.com>
2024-10-10 17:09:00 +0200

    comment typo

--- src/glassesTools/annotation.py
+++ src/glassesTools/annotation.py
@@ -9,7 +9,7 @@ class Type(Enum):
 class Event(utils.AutoName):
     Validate    = auto()    # interval to be used for running glassesValidator
     Sync_Camera = auto()    # point to be used for synchronizing different cameras
-    Sync_ET_Data= auto()    # episode to be used for synchronization of eye tracker data to sceen camera (e.g. using VOR)
+    Sync_ET_Data= auto()    # episode to be used for synchronization of eye tracker data to scene camera (e.g. using VOR)
     Trial       = auto()    # episode for which to map gaze to plane(s): output for files to be provided to user
 events = [x.value for x in Event]
 utils.register_type(utils.CustomTypeEntry(Event,'__enum.Event__',str, lambda x: getattr(Event, x.split('.')[1])))
