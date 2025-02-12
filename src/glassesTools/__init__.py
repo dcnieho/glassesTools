7ee087dca3e973a9b76fe7cf375e9c3e6ed8bb7a
Diederick Niehorster <dcnieho@gmail.com>
2025-02-12 14:58:43 +0100

    ensure ffmpeg binaries are available on every platform and on path

--- src/glassesTools/__init__.py
+++ src/glassesTools/__init__.py
@@ -2,3 +2,8 @@
 
 from . import importing
 from .version import __version__, __url__, __author__, __email__, __description__
+
+
+# ensure ffmpeg binaries needed by various submodules are on path
+import ffmpeg as _ffmpeg
+_ffmpeg.add_to_path()
