6353881a3247aadb08e93f24454d5f64b1c79a1e
Diederick Niehorster <dcnieho@gmail.com>
2025-04-10 13:24:35 +0200

    plane.Plane should return not only the markers but also the dict_id for each marker

--- src/glassesTools/plane.py
+++ src/glassesTools/plane.py
@@ -131,8 +131,8 @@ class Plane:
 
             self.markers[idx] = marker.Marker(idx, c, corners=[tl, tr, br, bl], rot=rot)
 
-    def get_marker_IDs(self) -> dict[str,list[int]]:
-        return {'plane': self._all_marker_ids}
+    def get_marker_IDs(self) -> dict[str,list[tuple[int,int]]]:
+        return {'plane': [(self.aruco_dict_id, m_id) for m_id in self._all_marker_ids]}
 
     def get_aruco_board(self) -> cv2.aruco.Board:
         from . import aruco
