8753df1a5f8d6a46557f325383f74cf9166e7d84
Diederick Niehorster <dcnieho@gmail.com>
2025-04-22 20:34:47 +0200

    bring back aruco.default_dict, still need it

--- src/glassesTools/aruco.py
+++ src/glassesTools/aruco.py
@@ -5,6 +5,8 @@ from typing import Any, TypedDict
 
 from . import drawing, ocv, plane, pose
 
+default_dict = cv2.aruco.DICT_4X4_250
+
 dict_to_str: dict[int,str] = {getattr(cv2.aruco,k):k for k in ['DICT_4X4_50', 'DICT_4X4_100', 'DICT_4X4_250', 'DICT_4X4_1000', 'DICT_5X5_50', 'DICT_5X5_100', 'DICT_5X5_250', 'DICT_5X5_1000', 'DICT_6X6_50', 'DICT_6X6_100', 'DICT_6X6_250', 'DICT_6X6_1000', 'DICT_7X7_50', 'DICT_7X7_100', 'DICT_7X7_250', 'DICT_7X7_1000', 'DICT_ARUCO_ORIGINAL', 'DICT_APRILTAG_16H5', 'DICT_APRILTAG_25H9', 'DICT_APRILTAG_36H10', 'DICT_APRILTAG_36H11', 'DICT_ARUCO_MIP_36H12']}
 
 # same number means that the dictionaries are the same (i.e. marker 3 is the same for all the dictionaries of the same family), just different number of markers in the dictionary
