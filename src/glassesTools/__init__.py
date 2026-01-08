3b1b65a1e410f11f4f852f67e8710b53ff0f56f8
Diederick Niehorster <dcnieho@gmail.com>
2026-01-08 11:35:39 +0100

    only add ffmpeg to path if it isn't on path already

--- src/glassesTools/__init__.py
+++ src/glassesTools/__init__.py
@@ -14,4 +14,5 @@ from .version import __version__, __url__, __author__, __email__, __description_
 
 # ensure ffmpeg binaries needed by various submodules are on path
 import ffmpeg as _ffmpeg
-_ffmpeg.add_to_path()
+if not _ffmpeg.is_on_path():
+    _ffmpeg.add_to_path()
