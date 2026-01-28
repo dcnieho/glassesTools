f4da92ced1c73edb423debdaf342822f39ac31af
Diederick Niehorster <dcnieho@gmail.com>
2026-01-28 18:07:15 +0100

    ocv.CV2VideoReader: keep more variables private

--- src/glassesTools/ocv.py
+++ src/glassesTools/ocv.py
@@ -100,28 +100,28 @@ class CV2VideoReader:
     def __init__(self, file: str|pathlib.Path, timestamps: list|np.ndarray|pd.DataFrame):
         self.file = pathlib.Path(file)
         if isinstance(timestamps,list):
-            self.ts = np.array(timestamps)
+            self._ts = np.array(timestamps)
         elif isinstance(timestamps, pd.DataFrame):
-            self.ts = timestamps['timestamp'].to_numpy()
+            self._ts = timestamps['timestamp'].to_numpy()
         else:
-            self.ts = timestamps
+            self._ts = timestamps
 
-        self.cap = cv2.VideoCapture(self.file)
-        if not self.cap.isOpened():
+        self._cap = cv2.VideoCapture(self.file)
+        if not self._cap.isOpened():
             raise RuntimeError('the file "{}" could not be opened'.format(str(self.file)))
-        self.nframes = len(self.ts)
+        self.nframes = len(self._ts)
         self.frame_idx = -1
         self._last_good_ts = (-1, -1., -1.)  # frame_idx, ts from opencv, ts from file
         self._cache: tuple[bool, np.ndarray, int, float] = None # self._cache[2] is frame index
 
     def __del__(self):
-        self.cap.release()
+        self._cap.release()
 
     def get_prop(self, cv2_prop):
-        return self.cap.get(cv2_prop)
+        return self._cap.get(cv2_prop)
 
     def set_prop(self, cv2_prop, val):
-        return self.cap.set(cv2_prop, val)
+        return self._cap.set(cv2_prop, val)
 
     # NB: we seek by spooling, because I found seeking through setting cv2.CAP_PROP_POS_MSEC unreliable
     def read_frame(self, report_gap=False, wanted_frame_idx:int=None) -> tuple[bool, np.ndarray, int, float]:
@@ -141,12 +141,12 @@ class CV2VideoReader:
             return self._cache
 
         while True:
-            ret, frame = self.cap.read()
+            ret, frame = self._cap.read()
             # get timestamp of this frame according to OpenCV
             # NB: this timestamp does not take into account edit lists in an mp4
             # it seems (compare with output of .\ffprobe.exe file.mp4 -select_streams 0 -show_entries frame=pts_time)
             # which is one of the reasons we provide our own timestamps
-            ocv_ts = self.cap.get(cv2.CAP_PROP_POS_MSEC)
+            ocv_ts = self._cap.get(cv2.CAP_PROP_POS_MSEC)
             self.frame_idx += 1
 
             # check if we're done. Can't trust ret==False to indicate we're at end of video, as
@@ -156,16 +156,16 @@ class CV2VideoReader:
                 return self._cache
 
             # keep going
-            ts_from_list = self.ts[self.frame_idx]
+            ts_from_list = self._ts[self.frame_idx]
             if self.frame_idx==1 or ocv_ts>0.:
                 # check for gap, and if there is a gap, fix up frame_idx if needed
                 if self._last_good_ts[0]!=-1 and ts_from_list-self._last_good_ts[2] < ocv_ts-self._last_good_ts[1]-1:  # little bit of leeway (1ms) for precision or mismatched timestamps
                     # we skipped some frames altogether, need to correct current frame_idx
                     t_jump = ocv_ts-self._last_good_ts[1]   # compare OpenCV timestamps to get size of jump
-                    tss = self.ts-self._last_good_ts[2]     # apply jump to our own timestamps (so, we're robust to e.g. OpenCV ignoring the edit list)
+                    tss = self._ts-self._last_good_ts[2]     # apply jump to our own timestamps (so, we're robust to e.g. OpenCV ignoring the edit list)
                     # find best matching frame idx so we catch up with the jump
                     self.frame_idx = self._find_closest_idx(t_jump, tss)
-                    ts_from_list = self.ts[self.frame_idx]
+                    ts_from_list = self._ts[self.frame_idx]
                     if report_gap and self.frame_idx-self._last_good_ts[0]>1:
                         print(f'Frame discontinuity detected (jumped from {self._last_good_ts[0]} to {self.frame_idx}), there are probably corrupt frames in your video')
                 self._last_good_ts = (self.frame_idx, ocv_ts, ts_from_list)
